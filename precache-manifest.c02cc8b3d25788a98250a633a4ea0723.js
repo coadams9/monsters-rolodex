049c685e973cb015525ce7cebf03aa6c3a937fc0
Cory <coadams9@gmail.com>
2019-07-28 01:08:50 -0400

    Updates

---
+++ precache-manifest.c02cc8b3d25788a98250a633a4ea0723.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce7506138bf892b09da6e869d56d6bd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "13222d683ed96adbab30",
+    "url": "/monsters-rolodex/static/css/main.a9230c41.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/monsters-rolodex/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "13222d683ed96adbab30",
+    "url": "/monsters-rolodex/static/js/main.73e381e7.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
