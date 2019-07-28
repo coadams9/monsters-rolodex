1349b24cce27c49bbd03a1811452849007f26ad7
Cory <coadams9@gmail.com>
2019-07-28 01:04:09 -0400

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 import './App.css';
 
 const API = 'https://jsonplaceholder.typicode.com/users'
 
 const App = props => {
   const [monsters, setMonsters] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchTerm.toLowerCase()))
 
   useEffect(() => {
     fetch(API)
@@ -15,7 +19,9 @@ const App = props => {
 
   return (
     <div className="App">
-      <CardList monsters={monsters} />
+      <h1>Monsters Rolodex</h1>
+      <SearchBox placeholder='search monsters' handleChange={e => setSearchTerm(e.target.value)} />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
